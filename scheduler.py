5ffa8961e4b13bea91f52bc12a5a55151194ff56
Dan Lindeman <lindemda@gmail.com>
2016-07-30 21:44:24 -0400

    initial drop

---
+++ scheduler.py
@@ -0,0 +1,59 @@
+import random
+"""
+        Room 1    Room 2
+      _____________________
+1:    |_________|_________|
+2:    |_________|_________|
+3:    |_________|_________|
+4;    |_________|_________|
+
+
+Talks: a, b, c, d, e, f, g, h
+Times; 1, 2, 3, 4
+
+{user_1: [a, b, c, d], user_2: [d, e, f, g]}
+"""
+
+# user_data = [{}, {}, {}]
+# #Counter.count()
+# vote_list = []
+# popularity_hash = Counter.count()
+
+# room = (Set(), Set())
+
+# def update(user_data):
+#     # add user data to the big_list
+#     # update the populatiry_hash
+#     pass
+
+
+# def validate():
+#     pass
+user_data = [
+ {'user1': ['e', 'd', 'b', 'h']},
+ {'user2': ['a', 'h', 'f', 'c']},
+ {'user3': ['e', 'h', 'd', 'b']},
+ {'user4': ['h', 'd', 'a', 'c']}]
+
+def update()
+
+for user in user_data:
+    print(user)
+# def generate_user(talks):
+#     user_choices = []
+#     for x in range(4):
+#         elem = random.choice(talks)
+#         user_choices.append(elem)
+#         talks.remove(elem)
+#     return user_choices
+
+
+# talks = ['a','b','c','d','e','f','g','h']
+# print(generate_user(talks))
+# talks = ['a','b','c','d','e','f','g','h']
+# print(generate_user(talks))
+# talks = ['a','b','c','d','e','f','g','h']
+
+# print(generate_user(talks))
+# talks = ['a','b','c','d','e','f','g','h']
+# print(generate_user(talks))
